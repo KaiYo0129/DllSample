44c47388c685ba4111d06c9516d61bc0e5a92af9
愷佑 簡 <yui852099@gmail.com>
2018-02-01 18:41:46 +0800

    加入專案檔案。

---
+++ DllSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary;
+
+namespace DllSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyRectangle rect = new MyRectangle();
+            rect.Height = 10;
+            rect.Witdh = 10;
+            Console.WriteLine("面積為 :" + rect.GetArea());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyLibrary/MyRectangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyLibrary
+{
+    public class MyRectangle
+    {
+        public int Witdh { get; set; }
+        public int Height { get; set; }
+        public int GetArea() { return Witdh * Height; }
+
+    }
+}
